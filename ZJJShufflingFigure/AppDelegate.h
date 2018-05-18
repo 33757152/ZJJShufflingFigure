ff5dcfff9055526e96eef63a3265eccc2ede214f
张锦江 <33757152@qq.com>
2018-05-18 09:10:44 +0800

    Initial Commit

---
+++ ZJJShufflingFigure/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZJJShufflingFigure
+//
+//  Created by 张锦江 on 2018/5/18.
+//  Copyright © 2018年 xtayqria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
